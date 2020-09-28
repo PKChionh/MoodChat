fda27994df8ad78a9a2fbca428eb69d2b003b514
Ubuntu <ubuntu@ip-172-31-31-9.ec2.internal>
2020-09-28 02:46:01 +0000

    Version 2

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 import json
 import pandas as pd
 from string import punctuation
@@ -9,23 +9,124 @@ from torch.utils.data import TensorDataset, DataLoader
 from torch import nn
 from torch import optim
 
+from wtforms import Form, TextField, SubmitField, validators
+
 app = Flask(__name__)
+
+class Sen_Analy_LSTM(nn.Module):
+
+  def __init__(self, n_vocab = 5401, n_embed = 50, n_hidden = 100, n_output = 1, n_layers = 2, drop_p = 0.8):
+    super().__init__()
+
+    self.n_vocab = n_vocab
+    self.n_layers = n_layers
+    self.n_hidden = n_hidden
+
+    self.embedding = nn.Embedding(n_vocab, n_embed)
+    self.lstm = nn.LSTM(n_embed, n_hidden, n_layers, batch_first = True, dropout = drop_p)
+    self.dropout = nn.Dropout(drop_p)
+    self.fc = nn.Linear(n_hidden, n_output)
+    self.sigmoid = nn.Sigmoid()
+
+  def init_hidden (self, batch_size):
+    device = "cpu"
+    weights = next(self.parameters()).data
+    h = (weights.new(self.n_layers, batch_size, self.n_hidden).zero_().to(device),
+          weights.new(self.n_layers, batch_size, self.n_hidden).zero_().to(device))
+
+    return h
+
+  def forward (self, input_words):
+    embedded_words = self.embedding(input_words)
+    lstm_out, h = self.lstm(embedded_words)
+    lstm_out = self.dropout(lstm_out)
+    lstm_out = lstm_out.contiguous().view(-1, self.n_hidden)
+    fc_out = self.fc(lstm_out)
+    sigmoid_out = self.sigmoid(fc_out)
+    sigmoid_out = sigmoid_out.view(batch_size, -1)
+    sigmoid_last = sigmoid_out[:, -1]
+
+    return sigmoid_last, h
+
+# end of class
+
+class InputForm(Form):
+
+  userInput1 = TextField("Enter a sentence", validators=[validators.InputRequired()])
+
+  submit = SubmitField("Enter")
 
 @app.route('/predict', methods=['GET'])
 def predict():
-  return "Predict"
+
+  def preprocess_review(review):
+
+    with open('models/word_to_int_dict.json') as handle:
+      word_to_int_dict = json.load(handle)
+
+    review = review.translate(str.maketrans('', '', punctuation)).lower().rstrip()
+      
+    tokenized = word_tokenize(review)
+
+    if len(tokenized) >= 50:
+      review = tokenized[:50]
+    else:
+      review= ['0']*(50-len(tokenized)) + tokenized
+
+    final = []
+
+    for token in review:
+      try:
+        final.append(word_to_int_dict[token])
+      except:
+        final.append(word_to_int_dict[''])
+
+    return final  
+
+
+  request_json = request.get_json()
+  i = request_json['input']
+
+  batch_size = 1
+
+  # Vocab = 5401
+  # Embed = 50
+  # Hidden = 100
+  # Output = 1
+  # Layers = 2
+  model = Sen_Analy_LSTM(5401, 50, 100, 1, 2)
+
+  model.load_state_dict(torch.load("models/model_nlp.pkl"))
+  model.eval()
+  words = np.array([preprocess_review(review=i)])
+  padded_words = torch.from_numpy(words)
+  pred_loader = DataLoader(padded_words, batch_size = 1, shuffle = True)
+  for x in pred_loader:
+    output = model(x)[0].item()
+
+  response = json.dumps({'response': output})
+  return response, 200
+#  return "Predict"
+
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-  return "Hello"
+
+  form = InputForm(request.form)
+
+  if request.method == 'POST' and form.validate():
+    txtUserInput = request.form['userInput1']
+
+  return render_template('index.html', form=form)
+#  return "Hello"
 
 @app.errorhandler(500)
 def internal_error(error):
-   return "500 error"
+  return "500 error"
 
 @app.errorhandler(404)
 def not_found(error):
-   return "404 error",404
+  return "404 error",404
 
 if __name__ == "__main__":
   print("Running server...")
